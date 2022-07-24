16f23b9ff06bd12b09ad4a618bd0ec774a854804
IrwanFicoFar <irwanhadi9@gmail.com>
2022-07-24 17:14:27 +0700

    penambahan generatNumber dan animasi

--- script.js
+++ script.js
@@ -13,17 +13,16 @@ let myGuntingComColor = document.getElementById("guntingCom-color")
 
 let myResult = document.getElementById("result")
 let myRefresh = document.getElementById("refresh")
-let myGeneratedNum = document.getElementById("quanity-percobaan")
+let myGenNum = document.getElementById("gen-num")
+
+let attempt = 0
 
 let myName = prompt("siapa nama anda ?")
 let h1Name = document.getElementById("player")
 h1Name.innerText = myName.toUpperCase()
-
-let number
-let random
+console.log(`yuk bermain gunting batu kertas ${myName.toUpperCase()}\n--------------------------`)
 
 // eventListener mouse in/out
-
 const eventListener = (img, color) =>{
     img.addEventListener("mouseenter", () => {
         img.style.cursor = "pointer"
@@ -41,7 +40,6 @@ let myEvent2 = eventListener(myKertasManImg, myKertasManColor)
 let myEvent3 = eventListener(myGuntingManImg, myGuntingManColor)
 
 // eventListener tombol refresh
-
 myRefresh.addEventListener("mouseenter", () => {
     myRefresh.style.cursor = "pointer"
     myRefresh.classList.add("animated")
@@ -56,158 +54,145 @@ myRefresh.addEventListener("click", () => {
     let reload = confirm ("yakin mau mengulang game ?")
     if (reload) {
     location.reload()
+    console.clear()
 } 
 })
 
-// change result display 
+// display generatedNumber ho many attempt
+const genNum = () => {
+    attempt
+    myGenNum.innerText = `kamu sudah mencoba ${attempt} kali`
+    myGenNum.style.color = "black"
+    myGenNum.classList.remove("gen-num")
+}
+
+// display generatedNumber when man win
+const genNumManWin = () => {
+    attempt
+    myGenNum.innerText = `yeaaa percobaan ke ${attempt} kamu menang !!!`
+    myGenNum.style.color = "green"
+    myGenNum.classList.add("gen-num")
+}
+
+// result display changing 
 const manWin = () => {
+    genNum()
+    myResult.innerText = `${myName.toUpperCase()} WIN`
     myResult.classList.remove("vs")
     myResult.classList.remove("final-result-com-win")
     myResult.classList.remove("final-result-draw")
     myResult.classList.add("final-result-man-win")
-    console.log(`Hasilnya ${myName.toUpperCase()} MENANG !!!`)
+    console.log(`>>> Hasilnya = ${myName.toUpperCase()} MENANG !!!`)
 }
 
 const comWin = () => {
+    genNum()
+    myResult.innerText = "COM WIN"
     myResult.classList.remove("vs")
     myResult.classList.remove("final-result-man-win")
     myResult.classList.remove("final-result-draw")
     myResult.classList.add("final-result-com-win")
-    console.log(`Hasilnya COM MENANG !!!`)
+    console.log(`>>> Hasilnya = COM MENANG !!!`)    
 }
 
 const draw = () => {
+    genNum()
+    myResult.innerText = "DRAW"
     myResult.classList.remove("vs")
     myResult.classList.remove("final-result-man-win")
     myResult.classList.remove("final-result-com-win")
     myResult.classList.add("final-result-draw")
-    console.log(`yahh sayang sekali Remis`)
+    console.log(`>>> Hasilnya = REMIS`)
 }
+
+// COM choose it self background changing
+const comChooseBatu = () => {
+    console.log("COM memilih batu")
+    myBatuComColor.style.backgroundColor = "grey"
+    myKertasComColor.style.backgroundColor = "transparent"
+    myGuntingComColor.style.backgroundColor = "transparent"
+}
+
+const comChooseKertas = () => {
+    console.log("COM memilih kertas")
+    myBatuComColor.style.backgroundColor = "transparent"
+    myKertasComColor.style.backgroundColor = "grey"
+    myGuntingComColor.style.backgroundColor = "transparent"
+}
+
+const comChooseGunting = () => {
+    console.log("COM memilih gunting")
+    myBatuComColor.style.backgroundColor = "transparent"
+    myKertasComColor.style.backgroundColor = "transparent"
+    myGuntingComColor.style.backgroundColor = "grey"
+}
+
 
 // on click to play game
 const clickBatuMan = () => {
     myBatuManColor.style.backgroundColor = "grey"    
-    number = 0
+    let number = 0
     let randNum = Math.floor(Math.random() * 3)
-    random = randNum
     console.log(`${myName.toUpperCase()} memilih batu`)
-    
-
-    if (randNum === 0) {
-        console.log("COM memilih batu")
-        myBatuComColor.style.backgroundColor = "grey"
-        myKertasComColor.style.backgroundColor = "transparent"
-        myGuntingComColor.style.backgroundColor = "transparent"
+    attempt++
+    if (randNum === number) {
+        comChooseBatu()
+        draw() 
     }
     else if (randNum === 1) {
-        console.log("COM memilih kertas")
-        myBatuComColor.style.backgroundColor = "transparent"
-        myKertasComColor.style.backgroundColor = "grey"
-        myGuntingComColor.style.backgroundColor = "transparent"
-    }
-    else {
-        console.log("COM memilih gunting")
-        myBatuComColor.style.backgroundColor = "transparent"
-        myKertasComColor.style.backgroundColor = "transparent"
-        myGuntingComColor.style.backgroundColor = "grey"
-    }
-
-    if (number === random) {
-        myResult.innerText = "DRAW"
-        draw()  
-    } 
-    else if ( random === 1) {
-        myResult.innerText = "COM WIN"
+        comChooseKertas()
         comWin()
     }
     else {
-        myResult.innerText = `${myName.toUpperCase()} WIN`
+        comChooseGunting()
         manWin()
+        genNumManWin()
     }
-
+    console.log(`attempt = ${attempt}\n--------------------------`)
 }
-
 
 const clickKertasMan = () => {
     myKertasManColor.style.backgroundColor = "grey"    
-    number = 1
+    let number = 1
     let randNum = Math.floor(Math.random() * 3)
-    random = randNum
     console.log(`${myName.toUpperCase()} memilih kertas`)
-    
-    if (randNum === 0) {
-        console.log("COM memilih batu")
-        myBatuComColor.style.backgroundColor = "grey"
-        myKertasComColor.style.backgroundColor = "transparent"
-        myGuntingComColor.style.backgroundColor = "transparent"
-    }
-    else if (randNum === 1) {
-        console.log("COM memilih kertas")
-        myBatuComColor.style.backgroundColor = "transparent"
-        myKertasComColor.style.backgroundColor = "grey"
-        myGuntingComColor.style.backgroundColor = "transparent"
-    }
-    else {
-        console.log("COM memilih gunting")
-        myBatuComColor.style.backgroundColor = "transparent"
-        myKertasComColor.style.backgroundColor = "transparent"
-        myGuntingComColor.style.backgroundColor = "grey"
-    }
-
-    if (number === random) {
-        myResult.innerText = "DRAW"
+    attempt++
+    if (randNum === number) {
+        comChooseKertas()
         draw()
-    } 
-    else if ( random === 0) {
-        myResult.innerText = `${myName.toUpperCase()} WIN`
-        manWin()
     }
-    else {
-        myResult.innerText = "COM WIN"
+    else if (randNum === 2){
+        comChooseGunting()
         comWin()
     }
-
+    else {
+        comChooseBatu()
+        manWin()
+        genNumManWin()
+    }
+    console.log(`attempt = ${attempt}\n--------------------------`)
 }
 
 const clickGuntingMan = () => {
     myGuntingManColor.style.backgroundColor = "grey"    
-    number = 2
+    let number = 2
     let randNum = Math.floor(Math.random() * 3)
-    random = randNum
     console.log(`${myName.toUpperCase()} memilih gunting`)
-
-    if (randNum === 0) {
-        console.log("COM memilih batu")
-        myBatuComColor.style.backgroundColor = "grey"
-        myKertasComColor.style.backgroundColor = "transparent"
-        myGuntingComColor.style.backgroundColor = "transparent"
-    }
-    else if (randNum === 1) {
-        console.log("COM memilih kertas")
-        myBatuComColor.style.backgroundColor = "transparent"
-        myKertasComColor.style.backgroundColor = "grey"
-        myGuntingComColor.style.backgroundColor = "transparent"
-    }
-    else {
-        console.log("COM memilih gunting")
-        myBatuComColor.style.backgroundColor = "transparent"
-        myKertasComColor.style.backgroundColor = "transparent"
-        myGuntingComColor.style.backgroundColor = "grey"
-    }
-
-    if (number === random) {
-        myResult.innerText = "DRAW"
+    attempt++
+    if (randNum === number){
+        comChooseGunting()
         draw()
-    } 
-    else if ( random === 1) {
-        myResult.innerText = `${myName.toUpperCase()} WIN`
-        manWin()
     }
-    else {
-        myResult.innerText = "COM WIN"
+    else if (randNum === 0){
+        comChooseBatu()
         comWin()
     }
-
+    else{
+        comChooseKertas()
+        manWin()
+        genNumManWin()
+    }    
+    console.log(`attempt = ${attempt}\n--------------------------`)
 }
 
 
